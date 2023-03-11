3557f1f039454818266def1ed43ecd5d1a5ff944
Michał Chmielewski <chmielewski.michal007@gmail.com>
2023-03-11 13:43:36 +0100

    create component title

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { AppComponent } from './app.component';
+import { TittleComponent } from './components/tittle/tittle.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TittleComponent
   ],
   imports: [
     BrowserModule,
